4a6d886fa6e00927e32bcd7f6835dad768c024e7
TonyZhangZTN <mcerztn@gmail.com>
2025-04-25 03:58:17 +0800

    update v0.1-rt

--- extract.sh
+++ extract.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -x
-tar xf Jetson_Linux_R36.4.3_aarch64.tbz2
-sudo tar xpf Tegra_Linux_Sample-Root-Filesystem_R36.4.3_aarch64.tbz2 -C Linux_for_Tegra/rootfs/
-tar xf public_sources_36.4.3.tbz2
-tar xf tar xf aarch64--glibc--stable-2022.08-1.tar.bz2
+tar xf Jetson_Linux_r36.4.3_aarch64.tbz2
+sudo tar xpf Tegra_Linux_Sample-Root-Filesystem_r36.4.3_aarch64.tbz2 -C Linux_for_Tegra/rootfs/
+tar xf public_sources.tbz2
+tar xf aarch64--glibc--stable-2022.08-1.tar.bz2 
 cd Linux_for_Tegra
 cd source
 #tar xf kernel_src.tbz2
